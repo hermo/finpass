ddbe615306db83dfb8764e451bc19e5004c2b5bf
Mirko Grönroos <mirko@mirko.fi>
2022-08-12 10:01:36 +0300

    Ensure that random part is always alphanumeric

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	parts := []string{randomWord(), randomWord(), randomWord(), randomStr()}
+	parts := []string{randomWord(), randomWord(), randomWord(), randomAlphaNumericString()}
 	x := randomInt(4)
 	parts[x], parts[3] = parts[3], parts[x]
 	passphrase := fmt.Sprintf("%s-%s-%s-%s", parts[0], parts[1], parts[2], parts[3])
@@ -29,11 +29,31 @@ func randomWord() string {
 	return word
 }
 
-func randomStr() string {
-	randomBytes := make([]byte, 4)
-	_, err := rand.Read(randomBytes)
-	if err != nil {
-		panic(err)
+func randomAlphaNumericString() string {
+	for {
+		randomBytes := make([]byte, 2)
+		_, err := rand.Read(randomBytes)
+		if err != nil {
+			panic(err)
+		}
+		s := base32.StdEncoding.EncodeToString(randomBytes)[:3]
+		if isAlphaNumeric(s) {
+			return s
+		}
 	}
-	return base32.StdEncoding.EncodeToString(randomBytes)[:3]
+}
+
+func isAlphaNumeric(s string) bool {
+	alpha := false
+	numeric := false
+
+	for _, r := range s {
+		if r >= 'A' && r <= 'Z' {
+			alpha = true
+		}
+		if r >= '0' && r <= '9' {
+			numeric = true
+		}
+	}
+	return alpha && numeric
 }
