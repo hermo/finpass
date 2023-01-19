da081e6568cdd1bc44700c01b774a3612dcdaa2f
Mirko Grönroos <hermo@dmz.fi>
2023-01-19 19:10:43 +0200

    Clean up AI silliness

--- entropy.rs
+++ entropy.rs
@@ -1,8 +1,6 @@
-// bruteForceEntropy calculates the entropy of a password based on the number of
-// characters and character classes used
+/// Calculate the entropy of a password
 pub fn bruteforce_entropy(password: &str) -> f64 {
-    // deduce character classes used from password. Classes include lowercase,
-    // uppercase, digits, and symbols
+    // Deduce character classes used from password.
     let mut lowercase = false;
     let mut uppercase = false;
     let mut digits = false;
@@ -33,21 +31,22 @@ pub fn bruteforce_entropy(password: &str) -> f64 {
     (password.len() as f64) * (characters as f64).log2()
 }
 
+/// Calculate the entropy of a password assuming the wordlist is known by the cracker
 pub fn wordlist_entropy(password: &str, separator: char, wordlist_size: usize) -> f64 {
-    // count the number of words in the password.
-    // exclude one word because it is assumed to be the random one.
+    // Count the number of words in the password, ignoring the alphanumeric section
     let word_count = password.chars().filter(|c| c == &separator).count();
     let words_ent = word_count as f64 * (wordlist_size as f64).log2();
 
-    // calculate entropy increase from the separator. there are 3 separators and they are always -
-    let separator_ent = 3f64.log2();
+    // Calculate entropy increase from the separator. There are 3 separators and they are always -
+    let separator_ent = 3_f64.log2();
 
-    // one word is always 3 characters long and contains uppercase letters and one or more digits
-    let random_ent = 3f64 * 36f64.log2();
+    // One word is always 3 characters long and contains uppercase letters and one or more digits
+    let random_ent = 3_f64 * 36_f64.log2();
 
     words_ent + separator_ent + random_ent
 }
 
+/// Estimate the time it would take to crack a password
 pub fn estimate_time_to_crack(entropy: f64) -> String {
     // Assume the cracking speed is 20MH/s
     // Estimate based on RTX 4090 Hashcat benchmarks
@@ -55,42 +54,24 @@ pub fn estimate_time_to_crack(entropy: f64) -> String {
     let guesses_per_second: f64 = 20e6;
     // calculate the number of guesses needed to crack the password
     let guesses: f64 = 2.0_f64.powf(entropy);
-    // calculate the number of seconds needed to crack the password
+
     let seconds: f64 = guesses / guesses_per_second;
-    // calculate minutes needed to crack the password
     let minutes: f64 = seconds / 60.0_f64;
-    // calculate hours needed to crack the password
     let hours: f64 = seconds / 60.0_f64 / 60.0_f64;
-    // calculate days needed to crack the password
     let days: f64 = seconds / 60.0_f64 / 60.0_f64 / 24.0_f64;
-    // calculate the number of years needed to crack the password
     let years: f64 = seconds / 60.0_f64 / 60.0_f64 / 24.0_f64 / 365.0_f64;
-    // calculate the number of centuries needed to crack the password
     let centuries: f64 = years / 100.0_f64;
-    // calculate thousands of years needed to crack the password
     let thousands_of_years: f64 = years / 1000.0_f64;
-    // calculate millions of years needed to crack the password
     let millions_of_years: f64 = years / 1000000.0_f64;
-    // calculate billions of years needed to crack the password
     let billions_of_years: f64 = years / 1000000000.0_f64;
-    // calculate trillions of years needed to crack the password
     let trillions_of_years: f64 = years / 1000000000000.0_f64;
-    // calculate quadrillions of years needed to crack the password
     let quadrillions_of_years: f64 = years / 1000000000000000.0_f64;
-    // calculate quintillions of years needed to crack the password
     let quintillions_of_years: f64 = years / 1000000000000000000.0_f64;
-    // calculate sextillions of years needed to crack the password
     let sextillions_of_years: f64 = years / 1000000000000000000000.0_f64;
-    // calculate septillions of years needed to crack the password
     let septillions_of_years: f64 = years / 1000000000000000000000000.0_f64;
-    // calculate octillions of years needed to crack the password
     let octillions_of_years: f64 = years / 1000000000000000000000000000.0_f64;
-    // calculate nonillions of years needed to crack the password
     let nonillions_of_years: f64 = years / 1000000000000000000000000000000.0_f64;
-    // return the appropriate number of years, centuries, millennia, or aeons as a formatted string with
-    // 0 decimal places until "years" when it switches to 1 decimal place. seconds is always 0 decimal places.
-    // start with the largest unit of time and work down to seconds.
-    // example: ~42 nonillion years
+
     if nonillions_of_years >= 1.0_f64 {
         format!("~{:.0} nonillion years", nonillions_of_years)
     } else if octillions_of_years >= 1.0_f64 {
--- main.rs
+++ main.rs
@@ -7,15 +7,19 @@ mod entropy;
 use entropy::{bruteforce_entropy, estimate_time_to_crack, wordlist_entropy};
 use words::WORDLIST;
 
-fn generate_password(max_length: usize) -> String {
+/// Generate a password.
+/// The returned password is a combination of three words and three random characters, separated by dashes.
+/// The max_length parameter allows limiting the length of the words used in the password. Use 0 to allow any length.
+fn generate_password(max_word_length: usize) -> String {
     let mut rng = rand::thread_rng();
-    let wordlist = get_words(max_length);
-    // Generate the first three words
+    let wordlist = get_words(max_word_length);
+
+    // Generate the word segments of the password
     let mut password_vec = (0..3)
         .map(|_| wordlist.choose(&mut rng).unwrap().to_string())
         .collect::<Vec<String>>();
 
-    // Generate the last three characters
+    // Generate the alphanumeric segment
     let mut segment = String::new();
     let mut has_char = false;
     let mut has_num = false;
@@ -36,15 +40,15 @@ fn generate_password(max_length: usize) -> String {
         }
     }
 
-    // Determine the position of the last three characters
+    // Determine position to insert alphanumeric segment
     let position = rng.gen_range(0..4);
 
-    // Combine the first three words and the last three characters
+    // Combine the segments
     password_vec.insert(position, segment);
     password_vec.join("-")
 }
 
-// Create function that returns words from WORDLIST having a maximum length max_length
+/// Get a list of words from the wordlist that are shorter than the max_length parameter.
 fn get_words(max_length: usize) -> Vec<&'static str> {
     if max_length == 0 {
         return WORDLIST.to_vec();
@@ -60,42 +64,46 @@ fn get_words(max_length: usize) -> Vec<&'static str> {
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Maximum length of each word in the password
-    #[arg(short, long, default_value_t = 0)]
-    max_length: u8,
-    /// Show entropy and estimated time to crack. The flag should be -i or --info
-    #[arg(short, long)]
-    info: bool,
+    #[arg(short = 'm', default_value_t = 0)]
+    max_word_length: u8,
+    /// Show information regarding entropy and estimated time to crack
+    #[arg(short = 'i', default_value_t = false)]
+    show_info: bool,
 }
 
 fn main() {
     let args = Args::parse();
-    if args.max_length != 0 && args.max_length < 3 {
+    if args.max_word_length != 0 && args.max_word_length < 3 {
         eprintln!("Maximum word length must be at least 3");
         std::process::exit(1);
     }
-    let password = generate_password(args.max_length as usize);
+
+    let password = generate_password(args.max_word_length as usize);
     println!("{}", password);
 
-    if args.info {
+    if args.show_info {
         let brute_ent = bruteforce_entropy(&password);
-        let words_ent = wordlist_entropy(&password, '-', WORDLIST.len());
+        let wordlist_ent = wordlist_entropy(&password, '-', WORDLIST.len());
+
         eprintln!("Entropy and estimated time to crack using a fast GPU-based attack (20 MH/s, one or more RTX 4090):");
         eprintln!(
             "* Brute-force:    {:5.1} bits ({})",
             brute_ent,
             estimate_time_to_crack(brute_ent)
         );
+
         eprintln!(
             "* Known wordlist: {:5.1} bits ({})",
-            words_ent,
-            estimate_time_to_crack(words_ent)
+            wordlist_ent,
+            estimate_time_to_crack(wordlist_ent)
         );
-        if args.max_length > 0 {
-            let words = get_words(args.max_length as usize);
+
+        if args.max_word_length > 0 {
+            let words = get_words(args.max_word_length as usize);
             let words_ent = wordlist_entropy(&password, '-', words.len());
             eprintln!(
                 "* Known wordlist and parameters (-m={}): {:5.1} bits ({})",
-                args.max_length,
+                args.max_word_length,
                 words_ent,
                 estimate_time_to_crack(words_ent)
             );
